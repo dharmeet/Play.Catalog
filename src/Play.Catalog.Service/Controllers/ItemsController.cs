937c8e0ad80d1530a2be2938939e97b100974f7c
Dharmeet Singh <dharmeetkhalsa@gmail.com>
2025-09-03 17:24:50 +0530

    Accessing the Play.Common nuget package and removing the redundant code.

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Repositories;
+using Play.Common;
 
 namespace Play.Catalog.Service.Controllers
 {
