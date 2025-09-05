4551bb4f2a7b782c92f174fa802648d08776beea
Dharmeet Singh <dharmeetkhalsa@gmail.com>
2025-09-05 13:33:24 +0530

    Using the code from nuget package now.

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -1,9 +1,6 @@
-using MassTransit;
-using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Settings;
+using Play.Common.MassTransit;
 using Play.Common.MongoDB;
-using Play.Common.Settings;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,18 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddMongo()
-    .AddMongoRepository<Item>("items");
-
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq((context, configurator) =>
-    {
-        var rabbitMqSettings = builder.Configuration.GetSection("RabbitMqSettings").Get<RabbitMqSettings>();
-        var serviceSettings = builder.Configuration.GetSection("ServiceSettings").Get<ServiceSettings>();
-        configurator.Host(rabbitMqSettings.Host);
-        configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-    });
-});
+    .AddMongoRepository<Item>("items")
+    .AddMassTransitWithRabbitMq();
 
 builder.Services.AddControllers(options =>
 {
--- src/Play.Catalog.Service/Settings/RabbitMqSettings.cs
+++
@@ -1,7 +0,0 @@
-namespace Play.Catalog.Service.Settings
-{
-    public class RabbitMqSettings
-    {
-        public string Host { get; init; }
-    }
-}
